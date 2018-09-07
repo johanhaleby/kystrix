e9bf2e699ab8988d11290151a89d590329e38f9e
Johan Haleby <johan.haleby@gmail.com>
2018-09-07 12:13:12 +0200

    Fixing build file

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,30 +1,31 @@
-import com.beust.kobalt.*
-import com.beust.kobalt.plugin.packaging.*
-import com.beust.kobalt.plugin.application.*
-import com.beust.kobalt.plugin.kotlin.*
+import com.beust.kobalt.plugin.packaging.assemble
+import com.beust.kobalt.project
 
 val p = project {
     name = "kystrix-core"
-    group = "com.example"
+    group = "se.haleby.kystrix"
     artifactId = name
-    version = "0.1"
+    version = "0.1.0"
+    val kotlinVersion = "1.2.61"
 
     dependencies {
-//        compile("com.beust:jcommander:1.68")
-        compile("org.jetbrains.kotlin:kotlin-runtime:1.2.10")
-        compile("org.jetbrains.kotlin:kotlin-stdlib:1.2.10")
+        compile("org.jetbrains.kotlin:kotlin-runtime:$kotlinVersion")
+        compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+        compile("com.netflix.hystrix:hystrix-core:1.5.12")
     }
 
     dependenciesTest {
-        compile("org.testng:testng:6.11")
+        compile("junit:junit:4.12")
+        compile("io.rest-assured:rest-assured:3.1.1")
+        compile("org.assertj:assertj-core:3.11.1")
+        compile("org.asynchttpclient:async-http-client:2.5.3")
+        compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.6")
+        compile("io.javalin:javalin:2.1.1")
+        compile("org.slf4j:slf4j-simple:1.7.25")
     }
 
     assemble {
         jar {
         }
-    }
-
-    application {
-        mainClass = "com.example.MainKt"
     }
 }
