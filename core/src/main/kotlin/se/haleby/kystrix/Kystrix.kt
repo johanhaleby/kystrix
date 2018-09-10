e09bef7346a480daf44a5ca01cc3ae768ad96d6a
Johan Haleby <johan.haleby@gmail.com>
2018-09-10 16:19:52 +0200

    Adding better docs

--- core/src/main/kotlin/se/haleby/kystrix/Kystrix.kt
+++ core/src/main/kotlin/se/haleby/kystrix/Kystrix.kt
@@ -3,49 +3,139 @@ package se.haleby.kystrix
 import com.netflix.hystrix.*
 import rx.Observable
 
-inline fun <reified T : Any>    hystrixObservableCommand(block: KystrixObservableCommand<T>.() -> Unit): Observable<T> = KystrixObservableCommand<T>().apply(block).build()
+/**
+ * This is the entry-point of the Kystrix DSL for [HystrixObservableCommand]'s.
+ *
+ *
+ * For example:
+ *
+ * ```kotlin
+ * val greetingObservable = hystrixObservableCommand<Greeting> {
+ *    groupKey("GreetingService")
+ *    commandKey("Greeting")
+ *    command {
+ *        // This is what you want Hystrix to wrap
+ *        get("/greeting?firstName=John&lastName=Doe").asJson().toObservable()
+ *    }
+ *    commandProperties {
+ *        withExecutionTimeoutInMilliseconds(10000)
+ *        withExecutionIsolationStrategy(THREAD)
+ *        withFallbackEnabled(false)
+ *    }
+ *    threadPoolProperties {
+ *        withQueueSizeRejectionThreshold(5)
+ *        withMaxQueueSize(10)
+ *    }
+ *}
+ * ```
+ *
+ * If you're using Spring you should also have a look at the [Kystrix Spring](http://kystrix.haleby.se) project.
+ */
+inline fun <reified T : Any> hystrixObservableCommand(block: KystrixObservableCommand<T>.() -> Unit): Observable<T> = KystrixObservableCommand<T>().apply(block).build()
 
+/**
+ * This is the entry-point of the Kystrix DSL for [HystrixCommand]'s.
+ *
+ *
+ * For example:
+ *
+ * ```kotlin
+ * val greeting = hystrixCommand<Greeting> {
+ *    groupKey("GreetingService")
+ *    commandKey("Greeting")
+ *    command {
+ *        // This is what you want Hystrix to wrap
+ *        get("/greeting?firstName=John&lastName=Doe").asJson()
+ *    }
+ *    commandProperties {
+ *        withExecutionTimeoutInMilliseconds(10000)
+ *        withExecutionIsolationStrategy(THREAD)
+ *        withFallbackEnabled(false)
+ *    }
+ *    threadPoolProperties {
+ *        withQueueSizeRejectionThreshold(5)
+ *        withMaxQueueSize(10)
+ *    }
+ *}.execute()
+ * ```
+ */
 inline fun <reified T : Any> hystrixCommand(block: KystrixCommand<T>.() -> Unit): HystrixCommand<T> = KystrixCommand<T>().apply(block).build()
 
-open class KystrixBase {
+/**
+ * A base-class for shared Kystrix DSL functionality. This should never be used directly.
+ */
+abstract class KystrixBase {
     internal lateinit var commandKey: HystrixCommandKey
     internal lateinit var groupKey: HystrixCommandGroupKey
     internal var commandProperties: HystrixCommandProperties.Setter = HystrixCommandProperties.Setter()
 
+    /**
+     * Specify the group key, see [HystrixCommandGroupKey].
+     */
     fun groupKey(key: HystrixCommandGroupKey) {
         groupKey = key
     }
 
+    /**
+     * Specify the group key as a String, see [HystrixCommandGroupKey].
+     */
     fun groupKey(key: String) {
         groupKey(HystrixCommandGroupKey.Factory.asKey(key))
     }
 
+    /**
+     * Specify the command key, see [HystrixCommandKey].
+     */
     fun commandKey(key: HystrixCommandKey) {
         commandKey = key
     }
 
+    /**
+     * Specify the command key as a String, see [HystrixCommandKey].
+     */
     fun commandKey(key: String) {
         commandKey(HystrixCommandKey.Factory.asKey(key))
     }
 
+    /**
+     * Configure the Hystrix command properties, see [HystrixCommandProperties].
+     */
     fun commandProperties(block: HystrixCommandProperties.Setter.() -> Unit) {
         commandProperties = HystrixCommandProperties.Setter().apply(block)
     }
 }
 
+/**
+ * Class used in the Kystrix DSL to define aspects of [HystrixObservableCommand]'s. Should not be used directly, rather refer to [hystrixObservableCommand].
+ */
 class KystrixObservableCommand<T> : KystrixBase() {
 
     private lateinit var command: () -> Observable<T?>
     private var fallback: (() -> Observable<T?>)? = null
 
+    /**
+     * Define the actual logic that will be called by Hystrix and thus wrapped in a circuit-breaker. This is typically an HTTP or database request.
+     *
+     * @param block The code block that will be executed by Hystrix.
+     * @see HystrixObservableCommand.construct
+     */
     fun command(block: () -> Observable<T?>) {
         command = block
     }
 
+    /**
+     * Define the logic that will be called when the circuit-breaker is open.
+     *
+     * @param block The code block that will be executed by Hystrix when the circuit-breaker is open.
+     * @see HystrixObservableCommand.resumeWithFallback
+     */
     fun fallback(block: () -> Observable<T?>) {
         fallback = block
     }
 
+    /**
+     * Build the [HystrixObservableCommand]. Should not be used directly, rather use the DSL [hystrixObservableCommand].
+     */
     fun build(): Observable<T> {
         val settings = HystrixObservableCommand.Setter.withGroupKey(groupKey)
                 .andCommandKey(commandKey)
@@ -57,6 +147,9 @@ class KystrixObservableCommand<T> : KystrixBase() {
     }
 }
 
+/**
+ * Class used in the Kystrix DSL to define aspects of [HystrixCommand]'s. Should not be used directly, rather refer to [hystrixCommand].
+ */
 class KystrixCommand<T> : KystrixBase() {
 
     private lateinit var command: () -> T?
@@ -64,26 +157,50 @@ class KystrixCommand<T> : KystrixBase() {
     private var definedFallback: (() -> T?)? = null
     private var threadPoolProperties: HystrixThreadPoolProperties.Setter = HystrixThreadPoolProperties.Setter()
 
+    /**
+     * Define the actual logic that will be called by Hystrix and thus wrapped in a circuit-breaker. This is typically an HTTP or database request.
+     *
+     * @param block The code block that will be executed by Hystrix.
+     * @see HystrixCommand.run
+     */
     fun command(block: () -> T?) {
         command = block
     }
 
+    /**
+     * Define the logic that will be called when the circuit-breaker is open.
+     *
+     * @param block The code block that will be executed by Hystrix when the circuit-breaker is open.
+     * @see HystrixCommand.getFallback
+     */
     fun fallback(block: () -> T?) {
         definedFallback = block
     }
 
+    /**
+     * Specify the thread-pool key, see [HystrixThreadPoolKey].
+     */
     fun threadPoolKey(key: HystrixThreadPoolKey) {
         threadPoolKey = key
     }
 
+    /**
+     * Specify the thread-pool key as a String, see [HystrixThreadPoolKey].
+     */
     fun threadPoolKey(key: String) {
         threadPoolKey(HystrixThreadPoolKey.Factory.asKey(key))
     }
 
+    /**
+     * Configure the Hystrix thread-pool properties, see [HystrixThreadPoolProperties].
+     */
     fun threadPoolProperties(block: HystrixThreadPoolProperties.Setter.() -> Unit) {
         threadPoolProperties = HystrixThreadPoolProperties.Setter().apply(block)
     }
 
+    /**
+     * Build the [HystrixCommand]. Should not be used directly, rather use the DSL [hystrixCommand].
+     */
     fun build(): HystrixCommand<T> {
         val settings = HystrixCommand.Setter.withGroupKey(groupKey)
                 .andCommandKey(commandKey)
--- spring/src/main/kotlin/se/haleby/kystrix/KystrixSpringWebflux.kt
+++ spring/src/main/kotlin/se/haleby/kystrix/KystrixSpringWebflux.kt
@@ -5,24 +5,117 @@ import reactor.core.publisher.Mono
 import rx.Observable
 import rx.RxReactiveStreams
 
+/**
+ * Extension function that enables calling a code block that returns a [Mono] in a *blocking* fashion when using the [hystrixCommand].
+ * For example:
+ *
+ * ```kotlin
+ * val greeting = hystrixCommand<Greeting> {
+ *    groupKey("Test")
+ *    commandKey("Test-Command")
+ *    // Here's the use of monoCommand
+ *    monoCommand {
+ *        webClient.get().uri("/somewhere").retrieve().bodyToMono()
+ *    }
+ *    commandProperties {
+ *        withExecutionTimeoutInMilliseconds(10000)
+ *        withExecutionIsolationStrategy(THREAD)
+ *        withFallbackEnabled(false)
+ *    }
+ *    threadPoolProperties {
+ *        withQueueSizeRejectionThreshold(5)
+ *        withMaxQueueSize(10)
+ *    }
+ *}.execute()
+ * ```
+ *
+ * **NOTE:** This command will block the Mono! You might want to look at using a [KystrixObservableCommand] (i.e. creating a DSL from [hystrixObservableCommand] instead).
+ */
 fun <T> KystrixCommand<T>.monoCommand(block: () -> Mono<T>) {
     command {
         block().block()
     }
 }
 
+/**
+ * Extension function that enables calling a code block that returns a [Mono] in a *non-blocking* fashion when using the [hystrixObservableCommand] DSL.
+ * For example:
+ *
+ * ```kotlin
+ * val greetingObservable = hystrixObservableCommand<Greeting> {
+ *    groupKey("Test")
+ *    commandKey("Test-Command")
+ *    // Here's the use of monoCommand
+ *    monoCommand {
+ *        webClient.get().uri("/somewhere").retrieve().bodyToMono()
+ *    }
+ *    commandProperties {
+ *        withExecutionTimeoutInMilliseconds(10000)
+ *        withExecutionIsolationStrategy(THREAD)
+ *        withFallbackEnabled(false)
+ *    }
+ *    threadPoolProperties {
+ *        withQueueSizeRejectionThreshold(5)
+ *        withMaxQueueSize(10)
+ *    }
+ *}
+ * ```
+ */
 fun <T> KystrixObservableCommand<T>.monoCommand(block: () -> Mono<T>) {
     command {
         Observable.from(block().toFuture())
     }
 }
 
+/**
+ * Extension function that enables calling a code block that returns a [Flux] in a *non-blocking* fashion when using the [hystrixObservableCommand] DSL.
+ *
+ * * For example:
+ *
+ * ```kotlin
+ * val greetingObservable = hystrixObservableCommand<Greeting> {
+ *    groupKey("Test")
+ *    commandKey("Test-Command")
+ *    // Here's the use of monoCommand
+ *    fluxCommand {
+ *        webClient.get().uri("/somewhere").retrieve().bodyToFlux()
+ *    }
+ *    commandProperties {
+ *        withExecutionTimeoutInMilliseconds(10000)
+ *        withExecutionIsolationStrategy(THREAD)
+ *        withFallbackEnabled(false)
+ *    }
+ *    threadPoolProperties {
+ *        withQueueSizeRejectionThreshold(5)
+ *        withMaxQueueSize(10)
+ *    }
+ *}
+ * ```
+ */
 fun <T> KystrixObservableCommand<T>.fluxCommand(block: () -> Flux<T>) {
     command {
         RxReactiveStreams.toObservable(block())
     }
 }
 
+/**
+ * Extension function converts an [Observable] into a [Mono]. Useful if you'd like to continue working with [Mono] data structures after you've used the Kystrix DSL ([hystrixObservableCommand]).
+ * For example:
+ *
+ * ```kotlin
+ * val greetingObservable : Observable<Greeting> = hystrixObservableCommand<Greeting> { ... }
+ * val greetingMono : Mono<Greeting> = greetingObservable.toMono()
+ * ```
+ */
 fun <T> Observable<T>.toMono(): Mono<T> = Mono.from(RxReactiveStreams.toPublisher(this))
 
+/**
+ * Extension function converts an [Observable] to a [Flux]. Useful if you'd like to continue working with [Flux] data structures after you've used the Kystrix DSL ([hystrixObservableCommand]).
+ * For example:
+ *
+ * ```kotlin
+ * val greetingObservable : Observable<Greeting> = hystrixObservableCommand<Greeting> { ... }
+ * val greetingFlux : Flux<Greeting> = greetingObservable.toFlux()
+ * ```
+ */
 fun <T> Observable<T>.toFlux(): Flux<T> = Flux.from(RxReactiveStreams.toPublisher(this))
