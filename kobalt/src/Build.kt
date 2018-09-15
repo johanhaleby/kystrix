266ba1a0e4c329755a5f13a23401d9879ab4873f
Johan Haleby <johan.haleby@gmail.com>
2018-09-15 07:22:06 +0200

    Extracted versions to an object

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -9,32 +9,52 @@ import org.apache.maven.model.License
 import org.apache.maven.model.Model
 import org.apache.maven.model.Scm
 
+object Versions {
+    // Java
+    const val java = "1.8"
+
+    // Compile versions
+    const val kystrix = "0.1.0"
+    const val kotlin = "1.2.61"
+    const val hystrix = "1.5.12"
+    const val reactorCore = "3.1.9.RELEASE"
+    const val reactiveStreams = "1.2.1"
+
+    // Test versions
+    const val assertj = "3.11.1"
+    const val slf4j = "1.7.25"
+    const val jackson = "2.9.6"
+    const val restAssured = "3.1.1"
+    const val asyncHttpClient = "2.5.3"
+    const val javalin = "2.1.1"
+    const val junit = "4.12"
+    const val reactorNetty = "0.7.8.RELEASE"
+    const val webflux = "5.0.8.RELEASE"
+}
+
 const val KYSTRIX_GROUP_NAME = "se.haleby.kystrix"
-const val KYSTRIX_VERSION = "0.1.0"
-const val KOTLIN_VERSION = "1.2.61"
-const val HYSTRIX_VERSION = "1.5.12"
 
 val core = project {
     name = "kystrix-core"
     group = KYSTRIX_GROUP_NAME
     artifactId = name
-    version = KYSTRIX_VERSION
+    version = Versions.kystrix
     directory = "core"
 
     dependencies {
-        compile("org.jetbrains.kotlin:kotlin-runtime:$KOTLIN_VERSION")
-        compile("org.jetbrains.kotlin:kotlin-stdlib:$KOTLIN_VERSION")
-        compile("com.netflix.hystrix:hystrix-core:$HYSTRIX_VERSION")
+        compile("org.jetbrains.kotlin:kotlin-runtime:${Versions.kotlin}")
+        compile("org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}")
+        compile("com.netflix.hystrix:hystrix-core:${Versions.hystrix}")
     }
 
     dependenciesTest {
-        compile("junit:junit:4.12")
-        compile("io.javalin:javalin:2.1.1")
-        compile("io.rest-assured:rest-assured:3.1.1")
-        compile("org.asynchttpclient:async-http-client:2.5.3")
-        compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.6")
-        compile("org.slf4j:slf4j-simple:1.7.25")
-        compile("org.assertj:assertj-core:3.11.1")
+        compile("junit:junit:${Versions.junit}")
+        compile("io.javalin:javalin:${Versions.javalin}")
+        compile("io.rest-assured:rest-assured:${Versions.restAssured}")
+        compile("org.asynchttpclient:async-http-client:${Versions.asyncHttpClient}")
+        compile("com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jackson}")
+        compile("org.slf4j:slf4j-simple:${Versions.slf4j}")
+        compile("org.assertj:assertj-core:${Versions.assertj}")
     }
 
     // Create maven pom so that we can sync with Maven central
@@ -64,28 +84,28 @@ val spring = project(core) {
     name = "kystrix-spring"
     group = KYSTRIX_GROUP_NAME
     artifactId = name
-    version = KYSTRIX_VERSION
+    version = Versions.kystrix
     directory = "spring"
 
     kotlinCompiler {
-        args("-jvm-target", "1.8")
+        args("-jvm-target", Versions.java)
     }
 
     dependencies {
-        compile("io.projectreactor:reactor-core:3.1.9.RELEASE")
-        compile("io.reactivex:rxjava-reactive-streams:1.2.1")
-        compile("org.jetbrains.kotlin:kotlin-runtime:$KOTLIN_VERSION")
-        compile("org.jetbrains.kotlin:kotlin-stdlib:$KOTLIN_VERSION")
-        compile("com.netflix.hystrix:hystrix-core:$HYSTRIX_VERSION")
+        compile("io.projectreactor:reactor-core:${Versions.reactorCore}")
+        compile("io.reactivex:rxjava-reactive-streams:${Versions.reactiveStreams}")
+        compile("org.jetbrains.kotlin:kotlin-runtime:${Versions.kotlin}")
+        compile("org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}")
+        compile("com.netflix.hystrix:hystrix-core:${Versions.hystrix}")
     }
 
     dependenciesTest {
-        compile("junit:junit:4.12")
-        compile("org.assertj:assertj-core:3.11.1")
-        compile("io.javalin:javalin:2.1.1")
-        compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.6")
-        compile("io.projectreactor.ipc:reactor-netty:0.7.8.RELEASE")
-        compile("org.springframework:spring-webflux:5.0.8.RELEASE")
+        compile("junit:junit:${Versions.junit}")
+        compile("org.assertj:assertj-core:${Versions.assertj}")
+        compile("io.javalin:javalin:${Versions.javalin}")
+        compile("com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jackson}")
+        compile("io.projectreactor.ipc:reactor-netty:${Versions.reactorNetty}")
+        compile("org.springframework:spring-webflux:${Versions.webflux}")
     }
 
     // Create maven pom so that we can sync with Maven central
