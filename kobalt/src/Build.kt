dfcbc3227724ead617eca7db45631fb0cb731711
Johan Haleby <johan.haleby@gmail.com>
2019-07-15 11:22:59 +0200

    Setting build version to 0.1.9-SNAPSHOT

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -14,7 +14,7 @@ object Versions {
     const val java = "1.8"
 
     // Kystrix
-    const val kystrix = "0.1.8"
+    const val kystrix = "0.1.9-SNAPSHOT"
 
     // Compile versions
     const val kotlin = "1.3.41"
